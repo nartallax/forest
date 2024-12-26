5871ee8fbd82f8df1728e134c4fcf6b65b8fb4dd
Nartallax <nartallax@gmail.com>
2024-12-26 02:00:41 +0100

    Use ts-build-utils

---
+++ build.mjs
@@ -0,0 +1,45 @@
+import {buildUtils} from "@nartallax/ts-build-utils"
+
+const {
+	npm, clear, typecheck, build, copyToTarget, cutPackageJson, runTests, generateDts
+} = buildUtils({
+	defaultBuildOptions: {
+		entryPoints: ["./src/forest.ts"],
+		bundle: true,
+		sourcemap: true,
+		platform: "neutral",
+		packages: "external",
+		format: "esm"
+	}
+})
+
+const main = async mode => {
+	switch(mode){
+		case "typecheck": {
+			await typecheck()
+		} break
+
+		case "build": {
+			await clear()
+			await build()
+			await generateDts()
+			await copyToTarget("README.md", "LICENSE")
+			await cutPackageJson()
+			console.log("Done.")
+		} break
+
+		case "test": {
+			await clear()
+			await runTests()
+		} break
+		
+		case "publish": {
+			await main("typecheck")
+			await main("test")
+			await main("build")
+			await npm.publish()
+		}
+	}
+}
+
+void main(process.argv[2])
